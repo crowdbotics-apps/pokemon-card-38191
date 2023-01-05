dcd1672af150c32bb1a27245a3c72838c3017414
Crowdbotics <team@crowdbotics.com>
2023-01-05 04:56:52 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "pokemon-card-38191",
+  url: "https://pokemon-card-38191.botics.co",
+  api: "https://pokemon-card-38191.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
